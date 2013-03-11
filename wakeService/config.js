4a0ee673f956220c7ad1a1194d16c472fdbc34c4
Alexander Shakhov <a.sahovs@diatom.lv>
2013-03-11 13:54:31 +0200

    Added wake port to config and removed kill handler.

--- wakeService/config.js
+++ wakeService/config.js
@@ -2,6 +2,7 @@
 module.exports = {
     ip: null,
     port: 8080,
+	wakePort: 9,
     secret: '123456',
     maxRequestAgeMinutes: 60
 }
--- wakeService/server.js
+++ wakeService/server.js
@@ -7,32 +7,31 @@ var config = require('./config.js');
 var app = express();
 
 
-app.post('/wake', express.bodyParser(), function(req, res) {
-    var request = {
-        mac: req.body.mac,
-        nonce: req.body.nonce,
-        timestamp: req.body.timestamp,
-        token: req.body.token
-    };
-    console.log(request);
-    
-    var validationResult = validator(request);
-    if (!validationResult.result) {
-        res.send('validation error - ' + validationResult.error);
-        return;
-    }
+app.post('/wake', express.bodyParser(), function (req, res) {
+	var request = {
+		mac:req.body.mac,
+		nonce:req.body.nonce,
+		timestamp:req.body.timestamp,
+		token:req.body.token
+	};
+	console.log(request);
 
-    wol.wake(request.mac, function(error) {
-        if (error) {
-            res.send('wake error - ' + error);
-        }
-        else {
-            res.send("OK");
-        }
-    });
+	var validationResult = validator(request);
+	if (!validationResult.result) {
+		res.send('validation error - ' + validationResult.error);
+		return;
+	}
+
+	wol.wake(request.mac, {port:config.wakePort}, function (error) {
+		if (error) {
+			res.send('wake error - ' + error);
+		}
+		else {
+			res.send("OK");
+		}
+	});
 });
-app.get('/kill', function(req, res) {
-    res.send('Killed');
-    process.exit();
+app.get('/test', function (req, res) {
+	res.send('ok');
 });
 app.listen(process.env.PORT || config.port, process.env.IP || config.ip);
